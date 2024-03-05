70e0711dd64e27df204463e8914ad9b7032fd246
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-03-05 11:16:46 +0530

    Webhook Triggered git push git status ;

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG21CS032 ADITHYA MAHESH has successfully built and run\n WEBHOOK TRIGGERED\n";
+  cout << "PES1UG21CS032 ADITHYA MAHESH has successfully built and run\nWEBHOOK TRIGGERED\n";
   return 0;
 }
