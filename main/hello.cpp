73429401d8f3064a00175e9ee1c1bd256de9a6ad
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-03-05 11:14:07 +0530

    Webhook Triggered

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG21CS032 ADITHYA MAHESH has successfully built and run\n WEBHOOK TRIGGERED";
+  cout << "PES1UG21CS032 ADITHYA MAHESH has successfully built and run\n WEBHOOK TRIGGERED\n";
   return 0;
 }
